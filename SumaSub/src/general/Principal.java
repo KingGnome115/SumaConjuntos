ae4d20a4d1aaedd96f2c574e2790de1e0fbce8fd
KingGnomo115 <kevinjec115@gmail.com>
2020-11-12 01:02:17 -0600

    La ventana ya acepta numeros +,- así como tener el limite de 10 elementos

--- SumaSub/src/general/Principal.java
+++ SumaSub/src/general/Principal.java
@@ -11,6 +11,7 @@ public class Principal extends javax.swing.JFrame
 
     //Saber el número de elementos que tendra el vector
     int cont = 0;
+    Vectores conjunto;
 
     /**
      * Creates new form Principal
@@ -34,6 +35,11 @@ public class Principal extends javax.swing.JFrame
         J1 = new javax.swing.JLabel();
         TAnadir = new javax.swing.JTextField();
         BAnadir = new javax.swing.JButton();
+        BCalcular = new javax.swing.JButton();
+        label1 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        JSub = new javax.swing.JLabel();
+        JSubSo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -58,20 +64,46 @@ public class Principal extends javax.swing.JFrame
             }
         });
 
+        BCalcular.setText("Calcular");
+        BCalcular.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                BCalcularActionPerformed(evt);
+            }
+        });
+
+        label1.setText("Subconjuntos");
+
+        jLabel1.setText("Subconjuntos Solucion");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(BCalcular)
+                .addGap(197, 197, 197))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(J1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(J1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(label1)
+                            .addComponent(jLabel1))
+                        .addGap(22, 22, 22)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(TAnadir, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(71, 71, 71)
+                                .addComponent(BAnadir))
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(JSubSo, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(JSub, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 132, Short.MAX_VALUE)))
                 .addContainerGap())
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(158, 158, 158)
-                .addComponent(TAnadir, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(71, 71, 71)
-                .addComponent(BAnadir)
-                .addContainerGap(138, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -82,7 +114,17 @@ public class Principal extends javax.swing.JFrame
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(TAnadir, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(BAnadir))
-                .addContainerGap(352, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(BCalcular)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(label1)
+                    .addComponent(JSub, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(JSubSo, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(258, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -108,13 +150,25 @@ public class Principal extends javax.swing.JFrame
 
     private void BAnadirActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_BAnadirActionPerformed
     {//GEN-HEADEREND:event_BAnadirActionPerformed
-
+        
+        if (cont <= 9)
+        {
+            cont++;
+            System.out.println(cont);
+        } else
+        {
+            if (cont == 10)
+            {
+                conjunto = new Vectores(cont);
+                BAnadir.setEnabled(false);
+                BCalcularActionPerformed(evt);
+            }
+        }
 
     }//GEN-LAST:event_BAnadirActionPerformed
 
     private void TAnadirKeyTyped(java.awt.event.KeyEvent evt)//GEN-FIRST:event_TAnadirKeyTyped
     {//GEN-HEADEREND:event_TAnadirKeyTyped
-
         if (TAnadir.getText().length() == 5)
         {
             evt.consume();
@@ -122,9 +176,16 @@ public class Principal extends javax.swing.JFrame
         {
             validaEntero(evt);
         }
-
     }//GEN-LAST:event_TAnadirKeyTyped
 
+    private void BCalcularActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_BCalcularActionPerformed
+    {//GEN-HEADEREND:event_BCalcularActionPerformed
+        
+        BAnadir.setEnabled(false);
+        JSub.setText(String.valueOf((int)Math.pow(cont, 2)));
+        
+    }//GEN-LAST:event_BCalcularActionPerformed
+    
     private void validaEntero(KeyEvent ke)
     {
         if ((ke.getKeyChar() < '0' || ke.getKeyChar() > '9')
@@ -182,8 +243,13 @@ public class Principal extends javax.swing.JFrame
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BAnadir;
+    private javax.swing.JButton BCalcular;
     private javax.swing.JLabel J1;
+    private javax.swing.JLabel JSub;
+    private javax.swing.JLabel JSubSo;
     private javax.swing.JTextField TAnadir;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel label1;
     // End of variables declaration//GEN-END:variables
 }
--- SumaSub/src/general/Vectores.java
+++ SumaSub/src/general/Vectores.java
@@ -1,7 +1,5 @@
 package general;
 
-import java.util.ArrayList;
-
 /**
  *
  * @author Kevin
