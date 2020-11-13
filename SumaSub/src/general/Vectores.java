6db938192de29facb34b6687adb1ba9f9cef28a1
KingGnomo115 <kevinjec115@gmail.com>
2020-11-13 03:17:27 -0600

    El programa ya hace las combinaciones de los elementos del conjunto así como regresa los subconjuntos solucion y los imprime en pantalla

--- SumaSub/src/general/Vectores.java
+++ SumaSub/src/general/Vectores.java
@@ -1,30 +1,36 @@
 package general;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *
  * @author Kevin
  */
 public class Vectores
 {
-    
+
     private int arr[];
+    public int cantidad = 0;
     private int tope = -1;
     private int pos = 0;
     private int neg = 0;
     private int zero = 0;
-    
+
     public Vectores(int n)
     {
         this.arr = new int[n];
     }
-    
+
     public void Inserta(int numero, int indice)
     {
         getArr()[indice] = numero;
         setTope(getTope() + 1);
         Posibilidades(numero);
     }
-    
+
     public String Desp()
     {
         String s = "{";
@@ -35,7 +41,7 @@ public class Vectores
         s += "}";
         return s;
     }
-    
+
     private void Posibilidades(int numero)
     {
         if (numero == 0)
@@ -52,7 +58,7 @@ public class Vectores
             }
         }
     }
-    
+
     public boolean HayResultados()
     {
         if (zero > 0 || (pos > 0 && neg > 0))
@@ -62,6 +68,61 @@ public class Vectores
         {
             return false;
         }
+    }
+
+    public String Subconjuntos()
+    {
+        String sub = "";
+
+        Set<Integer> set = new HashSet<Integer>();
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            set.add(arr[i]);
+            System.out.println("Se añadio: "+arr[i]);
+        }
+
+        for (Set<Integer> s : Powerset(set))
+        {
+            Object[] arreglo = s.toArray();
+            int cero = 0;
+            for (int i = 0; i < arreglo.length; i++)
+            {
+                cero += (int) arreglo[i];
+                if (i == arreglo.length - 1)
+                {
+                    if (cero == 0)
+                    {
+                        cantidad++;
+                        sub += s + "\n";
+                    }
+                }
+            }
+        }
+
+        return sub;
+    }
+
+    public static <T> Set<Set<T>> Powerset(Set<T> originalSet)
+    {
+        Set<Set<T>> sets = new HashSet<Set<T>>();
+        if (originalSet.isEmpty())
+        {
+            sets.add(new HashSet<T>());
+            return sets;
+        }
+        List<T> list = new ArrayList<T>(originalSet);
+        T head = list.get(0);
+        Set<T> rest = new HashSet<T>(list.subList(1, list.size()));
+        for (Set<T> set : Powerset(rest))
+        {
+            Set<T> newSet = new HashSet<T>();
+            newSet.add(head);
+            newSet.addAll(set);
+            sets.add(newSet);
+            sets.add(set);
+        }
+        return sets;
     }
 
     /**
